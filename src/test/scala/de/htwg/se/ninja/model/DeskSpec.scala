ed2e34f110933439f59c916fd184e86253a79f29
caroweng <caro.weng@gmx.net>
2019-10-11 10:44:59 +0200

    modify DeskSpec

--- src/test/scala/de/htwg/se/ninja/model/DeskSpec.scala
+++ src/test/scala/de/htwg/se/ninja/model/DeskSpec.scala
@@ -3,89 +3,18 @@ package de.htwg.se.ninja.model
 import org.scalatest.{Matchers, WordSpec}
 
 class DeskSpec extends WordSpec with Matchers{
-  "A desk" when {
-    val playerC = Player("helen", StateOfPlayer.pause)
-    val playerH = Player("caro", StateOfPlayer.go)
-    val desk = Desk(Field(Array.ofDim[Cell](3, 3)), playerC, playerH)
-    "new game set" should {
-      desk.setNewGame()
-      "have Cells without ninjas" in {
-        desk.field.matrix(1)(1) should be(Cell(None))
-      }
-      "have a ninja" in {
-        val n1 = desk.field.matrix(0)(2).optNinja.get
-        desk.field.matrix(0,2) should be (Cell(Some(n1)))
-      }
-    }
-    "position of a ninja" should {
-      val n1 = desk.field.matrix(0)(2).optNinja.get
-      val pos = desk.field.getPosition(n1)
-      "ninja be at position" in {
-        pos._1 should be(0)
-        pos._2 should be(2)
-        desk.field.getPosition(n1) should be((0, 2))
-      }
-    }
-    "its field" should {
-      val n1 = desk.field.matrix(0)(2).optNinja.get
-      val pos = desk.field.getPosition(n1)
-      val add = desk.field.add((0,1), pos)
-      val inbo = desk.field.inBounds(add)
-      "cell in field exists" in {
-        add._2 should be(3)
-        inbo should be(false)
-        desk.field.exists(pos._1, pos._2,  Direction.right) should be(false)
-      }
-    }
-    "a ninja walks" should {
-      val n1 = desk.field.matrix(0)(2).optNinja.get
-      "change its position" in {
-        val newDesk = desk.walk(playerC, n1, Direction.down)
-        newDesk.field.matrix(0,2).optNinja should be(None)
-        newDesk.field.matrix(1,2).optNinja should be(Some(n1))
-      }
-    }
+    "A desk" when {
+        val player1: Player = Player("helen", StateOfPlayer.go)
+        val player2: Player =  Player("caro", StateOfPlayer.pause)
+        val field: Field = Field(Array.ofDim[Cell](6,6))
+        val desk : Desk = Desk(field ,player1,player2)
 
-    " a ninja " should{
-      val ddd: Desk = desk.setFlag(playerC, 0, 0)
-      "with flag" in {
-        ddd.field.matrix(0,0).optNinja.get.weapon should be (Weapon.flag)
-      }
-    }
+        "be constructed with" in{
+            desk.player1 should be (player1)
+            desk.player2 should be (player2)
+            desk.field should be (field)
+        }
 
-    /*"a ninja" should{
-      val n3 = desk.field.matrix(2)(1).optNinja.get
-      val d4 = desk.setFlag(playerH, 2,0 )
-      "wins" in {
-        d4.field.matrix(2)(1).optNinja should be(Some(n3))
-        d4.win(d4.field.getPosition(n3)_1 , d4.field.getPosition(n3)_2, Direction.left) should be (true)
-      }
-    }*/
 
-    "a Player" should{
-      val einDesk = desk.changeTurns()
-      "have a turn" in {
-        desk.player1.state should be (StateOfPlayer.pause)
-        desk.player2.state should be (StateOfPlayer.go)
-      }
-      "change turn" in {
-        einDesk.player1.state should be(StateOfPlayer.go)
-        einDesk.player2.state should be(StateOfPlayer.pause)
-        print(desk.toString())
-        print(einDesk.toString())
-      }
     }
-//    "a Player" should {
-//      val name = "helen"
-//      "be identified by its name" in {
-//        desk.getPlayerWithName(name) should be(desk.player1)
-//      }
-//    }
-//    "a String" should {
-//      val s = "down"
-//      "become a direction" in {
-//        desk.toDirection(s) should be (Direction.down)
-//      }
-//    }
-  }
 }
